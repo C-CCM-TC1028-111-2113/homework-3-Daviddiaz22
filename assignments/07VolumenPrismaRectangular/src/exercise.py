8cddccbec561e87121a11e8ec93d15a6dd5fa6ad
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-29 19:22:45 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,7 +1,19 @@
+def area(b,h):
+    a = (b * h)
 
+    return a
+
+def vol(a,l):
+    v = a * l
+
+    return v
 def main():
     #escribe tu código abajo de esta línea
-    pass
+base = float(input('Dame la base: '))
+altura = float(input('Dame la altura: '))
+prof = float(input('Dame la profundidad: '))
+
+print('El volumen del prisma es: ' + str(vol(area(base, altura),prof)))
 
 if __name__=='__main__':
     main()
